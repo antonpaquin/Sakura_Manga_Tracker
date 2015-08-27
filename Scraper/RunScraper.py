cf0bc58350df337d48dcfcf92d848c0f3961101c
Anton Paquin <antonpaquin@gmail.com>
2015-08-26 21:51:25 -0700

    Server.py will also create some directories that should be there if they don't already exist

---
+++ Scraper/RunScraper.py
@@ -0,0 +1,3 @@
+import GrabOrchestrator
+
+GrabOrchestrator.run()
--- server.py
+++ server.py
@@ -4,12 +4,20 @@ import http.server
 import socketserver
 import os
 
+pages_l = os.listdir()
+if 'Users' not in pages_l:
+    os.mkdir('Users')
+if 'Pages' not in pages_l:
+    os.mkdir('Pages')
+if 'Conf' not in os.listdir('Scraper'):
+    os.mkdir('Scraper/Conf')
+
 port=8000
 
 Handler = http.server.CGIHTTPRequestHandler
 
 Handler.cgi_directories = ['/']
-    
+
 print(Handler.cgi_directories)
 
 httpd = http.server.HTTPServer(("", port), Handler)
